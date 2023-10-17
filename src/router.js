ebcb3a91c71bea0a417febe7c15c504c7b61c30a
Thomas-Basadonne <thommybasa@gmail.com>
2023-10-17 18:54:48 +0200

    fix router + edit home

--- src/router.js
+++ src/router.js
@@ -1,9 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "./views/Home.vue";
+// front
 import QuizHtml from "./views/front/QuizHtml.vue";
 import QuizCss from "./views/front/QuizCss.vue";
 import QuizJs from "./views/front/QuizJs.vue";
+import QuizScss from "./views/front/QuizScss.vue";
+// back
+import QuizPhp from "./views/back/QuizPhp.vue";
+import QuizJava from "./views/back/QuizJava.vue";
+import QuizPy from "./views/back/QuizPy.vue";
+import QuizRuby from "./views/back/QuizRuby.vue";
+// db
+import QuizSql from "./views/db/QuizSql.vue";
+import QuizMySql from "./views/db/QuizMySql.vue";
+import QuizMongoDB from "./views/db/QuizMongoDB.vue";
+import QuizRedis from "./views/db/QuizRedis.vue";
+// framework
+import QuizVue from "./views/framework/QuizVue.vue";
+import QuizReact from "./views/framework/QuizReact.vue";
+import QuizSpring from "./views/framework/QuizSpring.vue";
+import QuizLaravel from "./views/framework/QuizLaravel.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -13,6 +30,7 @@ const router = createRouter({
       name: "Home",
       component: Home,
     },
+    // Front-end
     {
       path: "/quiz-html",
       name: "QuizHtml",
@@ -27,6 +45,74 @@ const router = createRouter({
       path: "/quiz-js",
       name: "QuizJs",
       component: QuizJs,
+    },
+    {
+      path: "/quiz-scss",
+      name: "QuizScss",
+      component: QuizScss,
+    },
+    // Back-end
+    {
+      path: "/quiz-php",
+      name: "QuizPhp",
+      component: QuizPhp,
+    },
+    {
+      path: "/quiz-java",
+      name: "QuizJava",
+      component: QuizJava,
+    },
+    {
+      path: "/quiz-py",
+      name: "QuizPy",
+      component: QuizPy,
+    },
+    {
+      path: "/quiz-ruby",
+      name: "QuizRuby",
+      component: QuizRuby,
+    },
+    // Database
+    {
+      path: "/quiz-sql",
+      name: "QuizSql",
+      component: QuizSql,
+    },
+    {
+      path: "/quiz-mysql",
+      name: "QuizMySql",
+      component: QuizMySql,
+    },
+    {
+      path: "/quiz-mongodb",
+      name: "QuizMongoDB",
+      component: QuizMongoDB,
+    },
+    {
+      path: "/quiz-redis",
+      name: "QuizRedis",
+      component: QuizRedis,
+    },
+    // Framework
+    {
+      path: "/quiz-laravel",
+      name: "QuizLaravel",
+      component: QuizLaravel,
+    },
+    {
+      path: "/quiz-react",
+      name: "QuizReact",
+      component: QuizReact,
+    },
+    {
+      path: "/quiz-vue",
+      name: "QuizVue",
+      component: QuizVue,
+    },
+    {
+      path: "/quiz-spring",
+      name: "QuizSpring",
+      component: QuizSpring,
     },
   ],
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -38,6 +38,9 @@ export default {
             <router-link to="/quiz-css" class="my-router-link"
               >Vai al quiz CSS</router-link
             >
+            <router-link to="/quiz-scss" class="my-router-link"
+              >Vai al quiz SCSS</router-link
+            >
             <router-link to="/quiz-js" class="my-router-link"
               >Vai al quiz JavaScript</router-link
             >
@@ -50,14 +53,17 @@ export default {
           <div
             class="d-flex flex-column justify-content-between align-items-center"
           >
-            <router-link to="/quiz-one-piece" class="my-router-link"
+            <router-link to="/quiz-php" class="my-router-link"
               >Vai al quiz PHP</router-link
             >
-            <router-link to="/quiz-hells-paradise" class="my-router-link"
+            <router-link to="/quiz-java" class="my-router-link"
               >Vai al quiz JAVA</router-link
             >
-            <router-link to="/quiz-memento" class="my-router-link"
-              >Vai al quiz Memento</router-link
+            <router-link to="/quiz-py" class="my-router-link"
+              >Vai al quiz Python</router-link
+            >
+            <router-link to="/quiz-ruby" class="my-router-link"
+              >Vai al quiz Ruby</router-link
             >
           </div>
         </div>
@@ -70,14 +76,17 @@ export default {
           <div
             class="d-flex flex-column justify-content-between align-items-center"
           >
-            <router-link to="/quiz-one-piece" class="my-router-link"
+            <router-link to="/quiz-sql" class="my-router-link"
+              >Vai al quiz SQL</router-link
+            >
+            <router-link to="/quiz-mysql" class="my-router-link"
               >Vai al quiz MySQL</router-link
             >
-            <router-link to="/quiz-hells-paradise" class="my-router-link"
-              >Vai al quiz Hells Paradise</router-link
+            <router-link to="/quiz-mongodb" class="my-router-link"
+              >Vai al quiz MongoDB</router-link
             >
-            <router-link to="/quiz-memento" class="my-router-link"
-              >Vai al quiz Memento</router-link
+            <router-link to="/quiz-redis" class="my-router-link"
+              >Vai al quiz Redis</router-link
             >
           </div>
         </div>
@@ -89,14 +98,17 @@ export default {
           <div
             class="d-flex flex-column justify-content-between align-items-center"
           >
-            <router-link to="/quiz-one-piece" class="my-router-link"
-              >Vai al quiz One Piece</router-link
+            <router-link to="/quiz-laravel" class="my-router-link"
+              >Vai al quiz Laravel</router-link
             >
-            <router-link to="/quiz-hells-paradise" class="my-router-link"
-              >Vai al quiz Hells Paradise</router-link
+            <router-link to="/quiz-react" class="my-router-link"
+              >Vai al quiz React</router-link
             >
-            <router-link to="/quiz-memento" class="my-router-link"
-              >Vai al quiz Memento</router-link
+            <router-link to="/quiz-vue" class="my-router-link"
+              >Vai al quiz Vue</router-link
+            >
+            <router-link to="/quiz-spring" class="my-router-link"
+              >Vai al quiz Spring</router-link
             >
           </div>
         </div>
